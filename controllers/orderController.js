ca8e205148465b48c6c24639ad276e27ba3d4f8a
Ezra Tsai <ishow8166@icloud.com>
2020-10-26 16:48:33 +0800

    feat: Post order、POST order/cancel

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,7 @@
 const db = require('../models')
 const Order = db.Order
+const OrderItem = db.OrderItem
+const Cart = db.Cart
 
 let orderController = {
     getOrders: (req, res) => {
@@ -9,6 +11,48 @@ let orderController = {
             })
         })
     },
+    postOrder: (req, res) => {
+        return Cart.findByPk(req.body.cartId, { include: 'items' }).then(cart => {
+            return Order.create({
+                name: req.body.name,
+                address: req.body.address,
+                phone: req.body.phone,
+                shipping_status: req.body.shipping_status,
+                payment_status: req.body.payment_status,
+                amount: req.body.amount,
+            }).then(order => {
+
+                var results = [];
+                for (var i = 0; i < cart.items.length; i++) {
+                    console.log(order.id, cart.items[i].id)
+                    results.push(
+                        OrderItem.create({
+                            OrderId: order.id,
+                            ProductId: cart.items[i].id,
+                            price: cart.items[i].price,
+                            quantity: cart.items[i].CartItem.quantity,
+                        })
+                    );
+                }
+
+                return Promise.all(results).then(() =>
+                    res.redirect('/orders')
+                );
+
+            })
+        })
+    },
+    cancelOrder: (req, res) => {
+        return Order.findByPk(req.params.id, {}).then(order => {
+            order.update({
+                ...req.body,
+                shipping_status: '-1',
+                payment_status: '-1',
+            }).then(order => {
+                return res.redirect('back')
+            })
+        })
+    }
 }
 
 module.exports = orderController 
